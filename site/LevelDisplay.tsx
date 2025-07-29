22ba507b2619f406af71e22fe58e42bad4cbda38
seveibar <seveibar@gmail.com>
2025-07-29 11:38:46 -0700

    improve leveldisplay

--- site/LevelDisplay.tsx
+++ site/LevelDisplay.tsx
@@ -1,12 +1,41 @@
+import { getSvgFromGraphicsObject } from "graphics-debug"
 import { CssGrid } from "lib/index"
 
 export const LevelDisplay = ({ level }: { level: any }) => {
   const grid = new CssGrid(level)
+
+  const resultGraphics = grid.visualize()
+  resultGraphics.points ??= []
+  const maxX = grid.opts.containerWidth ?? 100
+  const maxY = grid.opts.containerHeight ?? 100
+  resultGraphics.points.push(
+    { x: 0, y: 0 },
+    { x: maxX, y: 0 },
+    { x: maxX, y: maxY },
+    { x: 0, y: maxY },
+  )
   // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ width: "50%" }}>
+          <h2>Browser Result</h2>
+          <div dangerouslySetInnerHTML={{ __html: grid.convertToHtml() }} />
+        </div>
+        <div>
+          <h2>Algorithm Result</h2>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: getSvgFromGraphicsObject(resultGraphics, {
+                backgroundColor: "white",
+                svgHeight: 200,
+                svgWidth: 200,
+              }),
+            }}
+          />
+        </div>
+      </div>
       <pre>{JSON.stringify(level, null, 2)}</pre>
-      <div dangerouslySetInnerHTML={{ __html: grid.convertToHtml() }} />
     </div>
   )
 }
