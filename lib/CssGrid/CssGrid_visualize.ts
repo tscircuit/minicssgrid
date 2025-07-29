d2fd3acc7d6f4d625ad61233c51079a0b530659a
seveibar <seveibar@gmail.com>
2025-07-28 19:21:36 -0700

    handwritten visualize implementation

--- lib/CssGrid/CssGrid_visualize.ts
+++ lib/CssGrid/CssGrid_visualize.ts
@@ -1,6 +1,66 @@
 import type { GraphicsObject } from "graphics-debug"
 import type { CssGrid } from "./CssGrid"
 
+const COLORS = [0, 1, 2, 3, 4, 5, 6, 7, 8].map(
+  (i) => `hsl(${(i * 360) / 9}deg, 100%, 50%)`,
+)
+
+const stringHash = (str: string): number => {
+  let hash = 0
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash << 5) - hash + str.charCodeAt(i)
+  }
+  return hash
+}
+
 export const CssGrid_visualize = (grid: CssGrid): GraphicsObject => {
-  throw new Error("Not implemented")
+  const layout = grid.layout()
+
+  const go: Required<GraphicsObject> = {
+    title: "CssGrid",
+    coordinateSystem: "screen",
+    lines: [],
+    circles: [],
+    points: [],
+    rects: [],
+    texts: [],
+  }
+
+  const { cells, rowSizes, columnSizes } = layout
+
+  for (const cell of cells) {
+    const { column, row, columnSpan, rowSpan, key } = cell
+
+    let x = 0
+    let y = 0
+
+    for (let i = 0; i < column; i++) {
+      x += columnSizes[i]!
+    }
+
+    for (let i = 0; i < row; i++) {
+      y += rowSizes[i]!
+    }
+
+    let width = 0
+    let height = 0
+
+    for (let i = column; i < column + columnSpan; i++) {
+      width += columnSizes[i]!
+    }
+
+    for (let i = row; i < row + rowSpan; i++) {
+      height += rowSizes[i]!
+    }
+
+    go.rects.push({
+      center: { x: x + width / 2, y: y + height / 2 },
+      width,
+      height,
+      color: COLORS[stringHash(key) % COLORS.length],
+      label: key,
+    })
+  }
+
+  return go
 }
--- tests/basics/basics01.test.ts
+++ tests/basics/basics01.test.ts
@@ -1,8 +1,12 @@
 import { expect, test } from "bun:test"
 import { CssGrid } from "lib/CssGrid/CssGrid"
 import basic01 from "testcases/basics/basics01"
+import { getSvgFromGraphicsObject } from "graphics-debug"
 
 test("minimal single-item layout", () => {
   const grid = new CssGrid(basic01)
-  console.log(grid.convertToHtml())
+
+  const go = grid.visualize()
+  const svg = getSvgFromGraphicsObject(go)
+  expect(svg).toMatchSvgSnapshot(import.meta.path)
 })
