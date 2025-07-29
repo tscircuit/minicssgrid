67381d510241fd9ef23254566ec2cd8a8896be54
seveibar <seveibar@gmail.com>
2025-07-28 19:34:57 -0700

    temp add game to extract test cases

---
+++ game.js
@@ -0,0 +1,247 @@
+var levels = [
+  {
+    name: 'grid-column-start 1',
+    board: 'c',
+    selector: '> :nth-child(1)',
+    style: {'grid-column-start': '3'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-start 2',
+    board: 'w',
+    selector: '> :nth-child(1)',
+    style: {'grid-column-start': '5'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#poison {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-end 1',
+    board: 'c',
+    classes: {'#garden > *, #plants > *': 'grid-column-start-1'},
+    selector: '> :nth-child(1)',
+    style: {'grid-column-end': '4'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n  grid-column-start: 1;\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-end 2',
+    board: 'c',
+    classes: {'#garden > *, #plants > *': 'grid-column-start-5'},
+    selector: '> :nth-child(1)',
+    style: {'grid-column-end': '2'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n  grid-column-start: 5;\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-end 3',
+    board: 'c',
+    classes: {'#garden > *, #plants > *': 'grid-column-start-1'},
+    selector: '> :nth-child(1)',
+    style: {'grid-column-end': '-2'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n  grid-column-start: 1;\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-start 3',
+    board: 'w',
+    selector: '> :nth-child(1)',
+    style: {'grid-column-start': '-3'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#poison {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-end 4',
+    board: 'c',
+    classes: {'#garden > *, #plants > *': 'grid-column-start-2'},
+    selector: '> :nth-child(1)',
+    style: {'grid-column-end': 'span 2'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n  grid-column-start: 2;\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-end 5',
+    board: 'c',
+    classes: {'#garden > *, #plants > *': 'grid-column-start-1'},
+    selector: '> :nth-child(1)',
+    style: {'grid-column-end': 'span 5'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n  grid-column-start: 1;\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-start 4',
+    board: 'c',
+    classes: {'#garden > *, #plants > *': 'grid-column-end-6'},
+    selector: '> :nth-child(1)',
+    style: {'grid-column-start': 'span 3'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n",
+    after: "  grid-column-end: 6;\n}"
+  },
+  {
+    name: 'grid-column 1',
+    board: 'c',
+    selector: '> :nth-child(1)',
+    style: {'grid-column': '4 / 6'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column 2',
+    board: 'c',
+    selector: '> :nth-child(1)',
+    style: {'grid-column': '2 / 5'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-row-start 1',
+    board: 'c',
+    selector: '> :nth-child(1)',
+    style: {'grid-row-start': '3'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-row-start 2',
+    board: 'c',
+    selector: '> :nth-child(1)',
+    style: {'grid-row': '3 / 6'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-row 1',
+    board: 'w',
+    selector: '> :nth-child(1)',
+    style: {'grid-column': '2', 'grid-row': '5'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#poison {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-column-row 2',
+    board: 'c',
+    selector: '> :nth-child(1)',
+    style: {'grid-column': '2 / 6', 'grid-row': '1 / 6'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-area 1',
+    board: 'c',
+    selector: '> :nth-child(1)',
+    style: {'grid-area': '1 / 2 / 4 / 6'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-area 2',
+    board: 'cc',
+    classes: {'#garden > :nth-child(2), #plants > :nth-child(2)': 'grid-column-4'},
+    selector: '> :nth-child(1)',
+    style: {'grid-area': '2 / 3 / 5 / 6'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water-1 {\n  grid-area: 1 / 4 / 6 / 5;\n}\n\n#water-2 {\n",
+    after: "}"
+  },
+  {
+    name: 'order 1',
+    board: 'cwccc',
+    selector: '> :nth-child(2)',
+    style: {'order': '2'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n.water {\n  order: 0;\n}\n\n#poison {\n",
+    after: "}"
+  },
+  {
+    name: 'order 2',
+    board: 'wcwcwcwcwc',
+    selector: '> :nth-child(odd)',
+    style: {'order': '-1'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n.water {\n  order: 0;\n}\n\n.poison {\n",
+    after: "}"
+  },
+  {
+    name: 'grid-template-columns 1',
+    board: 'c',
+    style: {'grid-template-columns': '50% 50%'},
+    before: "#garden {\n  display: grid;\n",
+    after: "  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n  grid-column: 1;\n  grid-row: 1;\n}"
+  },
+  {
+    name: 'grid-template-columns 2',
+    board: 'c',
+    classes: {'#plants': 'grid-template-columns-repeat-8-12'},
+    style: {'grid-template-columns': 'repeat(8, 12.5%)'},
+    before: "#garden {\n  display: grid;\n",
+    after: "  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n  grid-column: 1;\n  grid-row: 1;\n}"
+  },
+  {
+    name: 'grid-template-columns 4',
+    board: 'cwc',
+    classes: {'#plants': 'grid-template-columns-100px-3em-40p'},
+    style: {'grid-template-columns': '100px 3em 40%;'},
+    before: "#garden {\n  display: grid;\n",
+    after: "  grid-template-rows: 20% 20% 20% 20% 20%;\n}"
+  },
+  {
+    name: 'grid-template-columns 5',
+    board: 'wc',
+    classes: {'#plants': 'grid-template-columns-1fr-5fr'},
+    style: {'grid-template-columns': '1fr 5fr;'},
+    before: "#garden {\n  display: grid;\n",
+    after: "  grid-template-rows: 20% 20% 20% 20% 20%;\n}"
+  },
+  {
+    name: 'grid-template-columns 3',
+    board: 'cw',
+    classes: {
+      '#plants': 'grid-template-columns-50px-1fr-1fr-1fr-50px',
+      '#plants > :nth-child(1), #garden > :nth-child(1)': 'grid-area-1-1-6-2',
+      '#plants > :nth-child(2), #garden > :nth-child(2)': 'grid-area-1-5-6-6'
+    },
+    style: {'grid-template-columns': '50px 1fr 1fr 1fr 50px;'},
+    before: "#garden {\n  display: grid;\n",
+    after: "  grid-template-rows: 20% 20% 20% 20% 20%;\n}\n\n#water {\n  grid-area: 1 / 1 / 6 / 2;\n}\n\n#poison {\n  grid-area: 1 / 5 / 6 / 6;\n}"
+  },
+  {
+    name: 'grid-template-columns 6',
+    board: 'wcw',
+    classes: {'#plants': 'grid-template-columns-6', '#garden, #overlay': 'grid-template-rows-100p'},
+    style: {'grid-template-columns': '75px 3fr 2fr;'},
+    before: "#garden {\n  display: grid;\n",
+    after: "  grid-template-rows: 100%;\n}"
+  },
+  {
+    name: 'grid-template-rows 1',
+    board: 'c',
+    classes: {
+      '#plants': 'grid-template-rows-50px-0-0-0-1fr',
+      '#plants > :nth-child(1), #garden > :nth-child(1)': 'grid-area-5-1-6-6'
+    },
+    style: {'grid-template-rows': '1fr 100px;'},
+    before: "#garden {\n  display: grid;\n  grid-template-columns: 20% 20% 20% 20% 20%;\n",
+    after: "}\n\n#water {\n  grid-column: 1 / 6;\n  grid-row: 5 / 6;\n}"
+  },
+  {
+    name: 'grid-template 1',
+    board: 'c',
+    style: {'grid-template': '60% 1fr / 200px 1fr'},
+    before: "#garden {\n  display: grid;\n",
+    after: "}\n\n#water {\n  grid-column: 1;\n  grid-row: 1;\n}"
+  },
+  {
+    name: 'grid-template 2',
+    board: 'wc',
+    classes: {'#plants': 'grid-template-2'},
+    style: {'grid-template': '1fr 50px / 20% 1fr'},
+    before: "#garden {\n  display: grid;\n",
+    after: "}"
+  }
+];
+
+var levelWin = {
+  name: 'win',
+  board: '',
+  classes: {'#garden, #plants, #overlay': 'win'},
+  style: {},
+  before: "#pond {\n  display: flex;\n",
+  after: "}"
+};
