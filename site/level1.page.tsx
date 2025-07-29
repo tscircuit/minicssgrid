cf1d7193a1adcba730bfd6c3d40a7f2b35c7a6c4
seveibar <seveibar@gmail.com>
2025-07-28 20:35:58 -0700

    wip

--- site/level1.page.tsx
+++ site/level1.page.tsx
@@ -1,1 +1,9 @@
-export default () => "hello world"
+import level1 from "testcases/level1"
+import { CssGrid } from "lib/CssGrid/CssGrid"
+
+export default () => {
+  const grid = new CssGrid(level1)
+
+  // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+  return <div dangerouslySetInnerHTML={{ __html: grid.convertToHtml() }} />
+}
