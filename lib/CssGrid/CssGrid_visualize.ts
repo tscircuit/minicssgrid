61a2e2d772c58e3cbf3b842d2f770433c5152306
seveibar <seveibar@gmail.com>
2025-07-28 19:05:54 -0700

    wip

--- lib/CssGrid/CssGrid_visualize.ts
+++ lib/CssGrid/CssGrid_visualize.ts
@@ -1,5 +1,6 @@
+import type { GraphicsObject } from "graphics-debug"
 import type { CssGrid } from "./CssGrid"
 
-export const CssGrid_visualize = (grid: CssGrid) => {
+export const CssGrid_visualize = (grid: CssGrid): GraphicsObject => {
   throw new Error("Not implemented")
 }
